fb080c634a0247af953534292cec4e9efa197183
vitoriaeliza08 <vitoria.sgarbossa.santos@escola.pr.gov.br>
2025-06-25 12:16:04 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,18 @@
+const perguntas = [
+    {
+        enunciado:"Como você lida com o estresse?",
+        alternativas:[
+            {
+                texto: "a) Praticando exercícios físicos e/ou se ocupando com outra coisa",
+                afirmação: ""
+            },
+                        {
+                texto: "",
+                afirmação: ""
+            }
+        ]
+    },
+    {
+
+    }
+]
